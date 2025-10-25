ee464ab454ec039a7580e4aac383b3619669904a
Gleb Grinchik <super_gleb1996@mail.ru>
2025-10-25 09:51:23 +0200

    final fixes

--- src/routes/db_routes.py
+++ src/routes/db_routes.py
@@ -8,7 +8,7 @@ db_router = APIRouter()
 async def get_messages_incoming():
     cursor = connection.cursor()
 
-    query = "SELECT * FROM messages_em limit(10);"
+    query = "SELECT * FROM messages_incoming order by timestamp desc limit(10);"
     cursor.execute(query)
 
     results = cursor.fetchall()
@@ -18,7 +18,7 @@ async def get_messages_incoming():
 async def get_messages_em():
     cursor = connection.cursor()
 
-    query = "SELECT * FROM messages_em limit(10);"
+    query = "SELECT * FROM messages_em order by timestamp desc limit(10);"
     cursor.execute(query)
 
     results = cursor.fetchall()
@@ -28,7 +28,7 @@ async def get_messages_em():
 async def get_messages_sent():
     cursor = connection.cursor()
 
-    query = "SELECT * FROM messages_sent limit(10);"
+    query = "SELECT * FROM messages_sent order by timestamp desc limit(10);"
     cursor.execute(query)
 
     results = cursor.fetchall()
